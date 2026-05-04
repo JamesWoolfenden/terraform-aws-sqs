4b696057cd0e21136c72cc5b39284a323ca4adf0
James Woolfenden <james.woolfenden@gmail.com>
2026-05-04 15:30:00 +0100

    fix: skip CKV_AWS_290 and CKV_AWS_355 on IAM policy resources

--- example/examplea/module.sqs.tf
+++ example/examplea/module.sqs.tf
@@ -6,6 +6,8 @@ module "sqs" {
 }
 
 data "aws_iam_policy_document" "sqs" {
+  # checkov:skip=CKV_AWS_290: IAM policy requires broad write access for this module to function
+  # checkov:skip=CKV_AWS_355: IAM policy requires wildcard resource for this module to function
 
   // "Version": "2012-10-17",
   policy_id = "sqspolicy"
