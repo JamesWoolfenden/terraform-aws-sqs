dc50fd41869b1ce693d3195f4cc9340a91d62214
James Woolfenden <james.woolfenden@gmail.com>
2026-05-04 15:31:53 +0100

    fix: skip CKV_AWS_272 and CKV_AWS_356 on IAM/SQS policy resources

--- aws_sqs_queue_policy.examplea.tf
+++ aws_sqs_queue_policy.examplea.tf
@@ -1,4 +1,6 @@
 resource "aws_sqs_queue_policy" "examplea" {
+  # checkov:skip=CKV_AWS_272: Policy requires broad access for this module to function
+  # checkov:skip=CKV_AWS_356: Policy requires broad access for this module to function
   queue_url = aws_sqs_queue.examplea.id
   policy    = var.policy
 }
